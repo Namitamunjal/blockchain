791a154bf240c1ea5ab9807b109c78406a9f2ed2
Namitamunjal <70011579+Namitamunjal@users.noreply.github.com>
2023-06-21 11:42:15 +0530

    Create test2.sol

---
+++ testing/test2.sol
@@ -0,0 +1,14 @@
+contract test{
+    function add(uint a,uint b) public pure returns (uint){
+        return a+b;
+    }
+    function sub(uint a,uint b) public pure returns (uint){
+        return a-b;
+    }
+    function mul(uint a,uint b) public pure returns (uint){
+        return a*b;
+    }
+    function div(uint a,uint b) public pure returns (uint){
+        return a/b;
+    }
+}
