d290b3b9f8640d7e4328245a0af7e35cf788a843
Namitamunjal <70011579+Namitamunjal@users.noreply.github.com>
2023-06-21 11:14:44 +0530

    Create conditional.sol

---
+++ testing/conditional.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier:GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+// contract conditional{
+//     function exmaple(uint x) external pure returns (uint){
+//         // if(x>10){
+//         //     return 3;
+//         // }
+//         // else {
+//         //     return 4;
+//         // }
+
+
+//         //ternary operator 
+//         return x>10?3:4;
+//     }
+// }
